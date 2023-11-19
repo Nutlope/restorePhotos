3c763757dc5d8d2c79f8df5c53075922377d4add
nutlope <hassan4709@gmail.com>
2023-11-19 15:06:51 -0500

    added plausible analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import { Analytics } from "@vercel/analytics/react";
-import type { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
-import "../styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
+import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+import '../styles/globals.css';
+import PlausibleProvider from 'next-plausible';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <PlausibleProvider domain="restorephotos.io">
+        <Component {...pageProps} />
+      </PlausibleProvider>
       <Analytics />
     </SessionProvider>
   );
